588f8949732be1e919331b338e0f0f4c4934881a
Chen Yuxi <chenyuxi@do1.com.cn>
2020-09-21 09:56:32 +0800

    1、添加lombok依赖及其使用例

--- src/main/java/yuc/learn/java_web/controller/HelloWorldController.java
+++ src/main/java/yuc/learn/java_web/controller/HelloWorldController.java
@@ -3,6 +3,7 @@ package yuc.learn.java_web.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import yuc.learn.java_web.pojo.vo.StudentTest;
 
 @RestController
 @RequestMapping("/api")
@@ -11,6 +12,15 @@ public class HelloWorldController {
     @GetMapping("/helloworld")
     public String helloWorld(){
         return "Hello World!";
+    }
+
+    @GetMapping("/stu")
+    public String studentShow(){
+        StudentTest studentTest = new StudentTest();
+        studentTest.setName("yuc");
+        studentTest.setId(1001L);
+        studentTest.setGrade(100);
+        return studentTest.toString();
     }
 
 }
