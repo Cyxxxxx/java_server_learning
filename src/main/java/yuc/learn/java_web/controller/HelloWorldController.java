d47a542631f62b74b49b9054a043e273bb575446
Chen Yuxi <chenyuxi@do1.com.cn>
2020-09-21 15:31:22 +0800

    添加fastjson使用例

--- src/main/java/yuc/learn/java_web/controller/HelloWorldController.java
+++ src/main/java/yuc/learn/java_web/controller/HelloWorldController.java
@@ -1,5 +1,7 @@
 package yuc.learn.java_web.controller;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,6 +27,21 @@ public class HelloWorldController {
         studentTest.setId(1001L);
         studentTest.setGrade(100);
         return studentTest.toString();
+    }
+
+    @GetMapping("/ptj")
+    public String POJOToJson(){
+        StudentTestVo studentTest = new StudentTestVo();
+        studentTest.setName("yuc");
+        studentTest.setId(1001L);
+        studentTest.setGrade(100);
+        return JSON.toJSONString(studentTest);
+    }
+
+    @GetMapping("/jtp")
+    public String jsonToPOJO(){
+        String jsonStr = "{\"grade\":100,\"id\":1001,\"name\":\"yuc\"}";
+        return JSONObject.parseObject(jsonStr,StudentTestVo.class).toString();
     }
 
 }
